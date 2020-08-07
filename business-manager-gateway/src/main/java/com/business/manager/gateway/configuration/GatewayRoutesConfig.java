8839ebca1790261b417a38be6567ae15dc5a21dd
jonathan.perez <jonathan.perez@globant.com>
2020-08-07 17:58:49 -0500

    Modified the pom to store all artifacts in cloud folder

--- business-manager-gateway/src/main/java/com/business/manager/gateway/configuration/GatewayRoutesConfig.java
+++ business-manager-gateway/src/main/java/com/business/manager/gateway/configuration/GatewayRoutesConfig.java
@@ -1,5 +1,9 @@
 package com.business.manager.gateway.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayRoutesConfig {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Value("${api.empleado.version:v1}")
+    private String empleadoApiVersion;
+
+    @Value("${api.horario.version:v1}")
+    private String horarioApiVersion;
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        Integer bmHorarioPort  = discoveryClient.getInstances("BMHORARIO").stream().map(ServiceInstance::getPort).findAny().orElse(8001);
+        Integer bmEmpleadoPort  = discoveryClient.getInstances("BMEMPLEADO").stream().map(ServiceInstance::getPort).findAny().orElse(8000);
+
         return builder
                 .routes()
                 .route("BMEMPLEADO",
@@ -17,9 +33,19 @@ public class GatewayRoutesConfig {
                         r.path("/BMEMPLEADO/**")
                                 .filters(f ->
                                         f.rewritePath("/BMEMPLEADO/(?<remaining>.*)",
-                                                "/${remaining}"))
+                                                "/"+empleadoApiVersion+"/${remaining}"))
                                 .uri("lb://BMEMPLEADO")
-                        .metadata("management.port", "8000")
+                        .metadata("management.port", bmEmpleadoPort)
+                )
+                .route("BMHORARIO",
+                        r ->
+                                r.path("/BMHORARIO/**")
+                                        .filters(f ->
+                                                f.rewritePath("/BMHORARIO/(?<remaining>.*)",
+                                                        "/"+horarioApiVersion+"/${remaining}"))
+                                        .uri("lb://BMHORARIO")
+                                        .metadata("management.port", bmHorarioPort)
+
                 )
                 .build();
     }
